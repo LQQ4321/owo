5928db7a7948931b64956f4b5c24a5199a9e5897
LQQ4321 <2754294620@qq.com>
2023-09-19 20:49:29 +0800

    find a idea to update data with concurrency

--- db/mysqlInit.go
+++ db/mysqlInit.go
@@ -61,7 +61,7 @@ func cacheLoop() {
 	// 因为我们用不到返回值，所以不必写出这样：
 	// for v := range updateTicker.C {}
 	for range updateTicker.C {
-		updateFunc()
+		updateFunc() //还是说是 go updateFunc()???
 	}
 
 	// for {
@@ -83,3 +83,98 @@ func updateFunc() {
 		}(k)
 	}
 }
+
+
+// 如果你的需求中读操作远远多于写操作，并且希望使用通道来实现读写安全，
+// 你可以考虑使用读写锁（sync.RWMutex）和通道的组合来实现。
+// 读操作可以直接读取变量的值，写操作则需要通过通道来进行同步。
+
+// 下面是一个示例代码，演示了如何使用读写锁和通道来实现对变量的读写安全，
+// 其中读操作直接读取变量的值，写操作通过通道来进行同步：
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+// 可能产生的后果，一直有读操作，那么写操作可能就一直没有机会执行，
+// 也许的解决办法，弄一个具有缓存的管道，没读一次，就向该管道中传递一个值，
+// 待到管道满了以后，就不能在进行读操作
+// 顺序：
+// 1.向缓存管道获取一个令牌
+// 2.读锁锁上（可能存在多个并行的读操作：2 ~ 4 步）
+// 3.进行读操作
+// 4.读锁解锁
+// 5.写锁锁上(发生情况：缓存通道满了，新的读操作无法再将读锁锁上,之前存在的所有读锁也都将解锁)
+// 6.清空缓存通道
+// 7.进行写操作
+// 8.写锁解锁
+// 新的轮回
+type SafeVariable struct {
+	mu    sync.RWMutex//读写锁：读锁锁上的时候可以读，写锁锁上的时候不能读也不能写
+	value int
+	setCh chan int
+}
+
+func NewSafeVariable(initialValue int) *SafeVariable {
+	sv := &SafeVariable{
+		value: initialValue,
+		setCh: make(chan int),
+	}
+
+	go sv.updateValue()
+
+	return sv
+}
+
+func (sv *SafeVariable) GetValue() int {
+	sv.mu.RLock()
+	defer sv.mu.RUnlock()
+
+	return sv.value
+}
+
+func (sv *SafeVariable) SetValue(newValue int) {
+	sv.setCh <- newValue
+}
+
+func (sv *SafeVariable) updateValue() {
+	for {
+		value := <-sv.setCh
+
+		sv.mu.Lock()
+		sv.value = value
+		sv.mu.Unlock()
+	}
+}
+
+func main() {
+	sv := NewSafeVariable(0)
+
+	// 读取变量的值
+	go func() {
+		for {
+			value := sv.GetValue()
+			fmt.Println("Value:", value)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// 修改变量的值
+	go func() {
+		for i := 1; i <= 5; i++ {
+			sv.SetValue(i)
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	time.Sleep(10 * time.Second)
+}
+// 在上面的示例中，我们定义了一个 SafeVariable 结构体，其中包含一个读写锁、一个值变量和一个设置通道 setCh。
+// SafeVariable 提供了 GetValue 和 SetValue 方法来读取和修改变量的值。
+// 在 updateValue 方法中，我们通过通道 setCh 来接收写操作传递的新值，并在加锁的情况下更新变量的值。
+
+// 在主函数中，我们启动了两个协程，一个用于读取变量的值，并输出到控制台，另一个用于修改变量的值。
+// 通过使用读写锁和通道的组合，我们实现了对变量的读写安全，并保证了读操作的并发性能。
